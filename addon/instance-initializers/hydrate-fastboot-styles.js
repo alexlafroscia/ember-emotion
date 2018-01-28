5423bcf912bbf8b83d5cc8414b0296e91624fa89
Alex LaFroscia <alafroscia@netflix.com>
2018-01-28 13:45:09 -0800

    Avoid FOUS when using FastBoot
    Closes #2

---
+++ addon/instance-initializers/hydrate-fastboot-styles.js
@@ -0,0 +1,19 @@
+import { get } from '@ember/object';
+import { hydrate } from 'emotion';
+
+export const FASTBOOT_STYLE_CACHE_NAME = 'emotion-ids';
+
+export function initialize(appInstance) {
+  const fastboot = appInstance.lookup('service:fastboot');
+
+  if (fastboot && !get(fastboot, 'isFastBoot')) {
+    const shoebox = get(fastboot, 'shoebox');
+    const emotionIds = shoebox.retrieve(FASTBOOT_STYLE_CACHE_NAME);
+
+    hydrate(emotionIds);
+  }
+}
+
+export default {
+  initialize
+};
